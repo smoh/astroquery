cc450508b69affdf2f6a7793a7cd9606ef5a5b78
Juan Carlos Segovia <juancarlos.segovia@gmail.com>
2018-10-10 16:26:14 +0200

    Updated login capability (added prompt)

--- astroquery/utils/tap/core.py
+++ astroquery/utils/tap/core.py
@@ -1405,7 +1405,7 @@ class TapPlus(Tap):
                 print("Invalid user name")
                 return
         if password is None:
-            password = getpass.getpass()
+            password = getpass.getpass("Password: ")
             if password is None:
                 print("Invalid password")
                 return
